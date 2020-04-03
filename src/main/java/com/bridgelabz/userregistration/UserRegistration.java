004354e4e775169034623e4a3ebcd2a8e9ba1ba0
vivek kumar <vivekhaarit@gmail.com>
2020-04-03 09:44:16 +0530

    email validation uploaded

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -5,9 +5,14 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     private static final String firstNamePattern = "^[A-Z]{1}[a-z]{2,}";
+    private static final String emailPattern = "^[a-z]{1,}[@](bl|gmail|yahoo|1|abc)(.com|.co|.net)$";
 
     public boolean firstName(String firstName) {
         Pattern pattern = Pattern.compile(firstNamePattern);
         return pattern.matcher(firstName).matches();
+    }
+    public boolean emailValidation(String emailID){
+        Pattern pattern = Pattern.compile(emailPattern);
+        return pattern.matcher(emailID).matches();
     }
 }
---
+++ src/test/java/com/bridgelabz/userregistration/EmailValidatorTest.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.userregistration;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailValidatorTest {
+    private String email;
+    private boolean expectedResult;
+    private UserRegistration validate;
+
+    public EmailValidatorTest(String email, boolean expectedResult){
+        this.email=email;
+        this.expectedResult=expectedResult;
+    }
+    @Before
+    public void initialize(){
+        validate = new UserRegistration();
+    }
+
+    @Parameterized.Parameters
+    public static Collection data(){
+        return Arrays.asList(new Object[][] {
+                {"vivekhaarit@gmail.com",true},
+                {"adfgsdgfdgg@gmail.com",true}
+        });
+    }
+
+    @Test
+    public void emailTest() {
+        Assert.assertEquals(expectedResult,validate.emailValidation(email));
+    }
+}
